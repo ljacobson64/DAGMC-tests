8d1edd4fad296a8ca706b516e58158e212549b79
Lucas Jacobson <ljacobson64@gmail.com>
2015-11-11 17:06:55 -0600

    added strip_geom_from_inp

--- Regression/setup_tests.py
+++ Regression/setup_tests.py
@@ -2,12 +2,13 @@ from subprocess import call
 import os
 
 def call_shell(string):
-    #print string
+    print string
     call(string, shell = True)
 
 class mcnp_test:
     def __init__(self, test_id):
-        self.input_dir = "Inputs"
+        self.input_nat_dir = "Inputs_native"
+        self.input_dag_dir = "Inputs_dagmc"
         self.sat_dir = "Geom_sat"
         self.h5m_dir = "Geom_h5m"
         self.log_dir = "Logs"
@@ -26,32 +27,58 @@ class mcnp_test:
         
         self.geomver = "1902"
         self.ftol = "1e-4"
+
     def __repr__(self):
         return "inp: " + self.inp
+
     def __str__(self):
         return "inp: " + self.inp
+
+    def strip_geom_from_inp(self):
+        reader = open(os.path.join(self.input_nat_dir, self.inp), 'r')
+        lines = reader.readlines()
+        reader.close()
+
+        call_shell("mkdir -p " + self.input_dag_dir)
+        writer = open(os.path.join(self.input_dag_dir, self.inp), 'w')
+        writer.write(lines[0])
+        num_blank_lines = 0
+        for line in lines[1:]:
+            if num_blank_lines < 2:
+                writer.write("c " + line)
+            else:
+                writer.write(line)
+            if line.strip() == "":
+                num_blank_lines += 1
+        writer.close()
+
     def run_mcnp2cad(self):
         call_shell("mkdir -p " + self.sat_dir)
         call_shell("mkdir -p " + self.log_dir)
-        call_shell("mcnp2cad " + self.input_dir + "/" + self.inp +
-                   " -o " + self.sat_dir + "/" + self.sat +
+        call_shell("mcnp2cad " + os.path.join(self.input_nat_dir, self.inp) +
+                   " -o " + os.path.join(self.sat_dir, self.sat) +
                    " --geomver=" + self.geomver +
-                   " 2>&1 | tee -a " + self.log_dir + "/" + self.satlog)
+                   " 2>&1 | tee -a " + os.path.join(self.log_dir, self.satlog))
+
     def run_dagmc_preproc(self):
         call_shell("mkdir -p " + self.h5m_dir)
         call_shell("mkdir -p " + self.log_dir)
-        call_shell("dagmc_preproc " + self.sat_dir + "/" + self.sat +
-                   " -o " + self.h5m_dir + "/" + self.h5m +
+        call_shell("dagmc_preproc " + os.path.join(self.sat_dir, self.sat) +
+                   " -o " + os.path.join(self.h5m_dir, self.h5m) +
                    " -f " + self.ftol +
-                   " 2>&1 | tee -a " + self.log_dir + "/" + self.h5mlog)
+                   " 2>&1 | tee -a " + os.path.join(self.log_dir, self.h5mlog))
+
     def setup_result_dir(self):
         call_shell("mkdir -p " + self.result_dir)
-        call_shell("ln -sf ../../" + self.input_dir + "/" + self.inp +
-                   " " + self.result_dir + "/" + self.inp)
-        call_shell("ln -sf ../../" + self.h5m_dir + "/" + self.h5m +
-                   " " + self.result_dir + "/" + self.h5m)
-        call_shell("ln -sf ../../" + self.xsdir_dir + "/" + self.xsdir +
-                   " " + self.result_dir + "/" + self.xsdir)
+        call_shell("ln -sf " + os.path.join("../..", self.input_dag_dir, self.inp) +
+                   " " + os.path.join(self.result_dir, self.inp))
+        call_shell("ln -sf " + os.path.join("../..", self.h5m_dir, self.h5m) +
+                   " " + os.path.join(self.result_dir, self.h5m))
+        call_shell("ln -sf " + os.path.join("../..", self.xsdir_dir, self.xsdir) +
+                   " " + os.path.join(self.result_dir, self.xsdir))
+        call_shell("ln -sf " + os.path.join("../..", self.xsdir_dir, self.xslib) +
+                   " " + os.path.join(self.result_dir, self.xslib))
+
     def call_mcnp(self, prun, mcnp):
         call_mcnp_str = prun + " " + mcnp
         if self.inp != "":
@@ -61,13 +88,13 @@ class mcnp_test:
         if self.xsdir != "":
             call_mcnp_str += " xsdir=" + self.xsdir
         call_mcnp_str += self.options
-        print call_mcnp_str
-        #call_shell(call_mcnp_str)
+        call_shell(call_mcnp_str)
 
 original_dir = os.getcwd()
 
-prun  = "mpiexec -np 24"
-mcnp  = "mcnp5.mpi"
+#prun = "mpiexec -np 24"
+prun = ""
+mcnp = "mcnp5.mpi"
 
 test_ids = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
@@ -79,16 +106,22 @@ test_ids = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
 test_ids_fatal = ["01", "02", "07", "11", "12", "18", "19", "20", "21", "23",
                   "30", "33", "77", "89"]
 
-#for test_id in test_ids:
-for test_id in test_ids:
+for test_id in [test_ids[0]]:
     test = mcnp_test(test_id)
+
     test.xsdir_dir = "../xsec_data"
     test.xsdir = "testdir1"
+    test.xslib = "testlib1"
     if test_id in test_ids_fatal:
         test.options += " fatal"
-    #test.run_mcnp2cad()
-    #test.run_dagmc_preproc()
+
+    test.strip_geom_from_inp()
+
     test.setup_result_dir()
+
+    test.run_mcnp2cad()
+    test.run_dagmc_preproc()
+
     os.chdir(test.result_dir)
     test.call_mcnp(prun, mcnp)
     os.chdir(original_dir)
