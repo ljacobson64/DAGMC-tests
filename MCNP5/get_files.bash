77b067cc5538caf359ce6a1354b4a3d26e8b1ef5
Lucas Jacobson <ljacobson64@gmail.com>
2016-01-29 15:53:39 -0600

    updated get_files script for MCNP

--- MCNP5/get_files.bash
+++ MCNP5/get_files.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-declare -a tarballs=(Geom_all_Meshtally.tar.gz \
-                     Geom_h5m_DAGMC.tar.gz \
-                     Geom_h5m_Regression.tar.gz \
-                     Geom_h5m_VALIDATION_CRITICALITY.tar.gz \
-                     Geom_h5m_VALIDATION_SHIELDING.tar.gz \
-                     Geom_h5m_VERIFICATION_KEFF.tar.gz \
-                     Geom_sat_DAGMC.tar.gz \
-                     Geom_sat_Regression.tar.gz \
-                     Geom_sat_VALIDATION_CRITICALITY.tar.gz \
-                     Geom_sat_VALIDATION_SHIELDING.tar.gz \
-                     Geom_sat_VERIFICATION_KEFF.tar.gz \
-                     xsec_data.tar.gz)
+tarballs="MCNP5_Geom_all_Meshtally.tar.gz
+          MCNP5_Geom_h5m_DAGMC.tar.gz
+          MCNP5_Geom_h5m_Regression.tar.gz
+          MCNP5_Geom_h5m_VALIDATION_CRITICALITY.tar.gz
+          MCNP5_Geom_h5m_VALIDATION_SHIELDING.tar.gz
+          MCNP5_Geom_h5m_VERIFICATION_KEFF.tar.gz
+          MCNP5_Geom_sat_DAGMC.tar.gz
+          MCNP5_Geom_sat_Regression.tar.gz
+          MCNP5_Geom_sat_VALIDATION_CRITICALITY.tar.gz
+          MCNP5_Geom_sat_VALIDATION_SHIELDING.tar.gz
+          MCNP5_Geom_sat_VERIFICATION_KEFF.tar.gz
+          MCNP5_xsec_data.tar.gz"
 
-for tarball in ${tarballs[@]}; do
+for tarball in $tarballs; do
   wget cnergdata.engr.wisc.edu/DAGMC-tests/tarballs/$tarball
   tar -xzvf $tarball
   rm -f $tarball
