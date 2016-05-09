47f663e5fe35c27808333dbe4354c187107b5870
Lucas John Jacobson <ljjacobson@wisc.edu>
2016-05-09 15:26:36 -0500

    updated scripts

--- DAG-MCNP5/scripts/run_dagmc_preproc.py
+++ DAG-MCNP5/scripts/run_dagmc_preproc.py
@@ -1,34 +1,38 @@
 import argparse
+import commands
+import os
 import sys
 from subprocess import call
 import multiprocessing as mp
 
-def run_dagmc_preproc(inp_file):
-    sat_file = "geom_" + inp_file + ".sat"
-    h5m_file = "geom_" + inp_file + ".h5m"
-    call("dagmc_preproc " + sat_file + " -o " + h5m_file, shell = True)
+def run_dagmc_preproc(sat_file):
+    dirname = os.path.dirname(os.path.dirname(sat_file))
+    name = os.path.basename(sat_file)[:-4]
+    h5m_file = os.path.join(dirname, 'Geom_h5m', name + '.h5m')
+    if not os.path.exists(os.path.join(dirname, 'Geom_h5m')):
+        os.makedirs(os.path.join(dirname, 'Geom_h5m'))
+    call('dagmc_preproc ' + sat_file + ' -o ' + h5m_file, shell = True)
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Run dagmc_preproc.")
-    parser.add_argument("files", nargs = "*",
-                        help = "files on which to run dagmc_preproc")
-    parser.add_argument("-j", "--jobs", type = int, default = 1,
-                        help = "number of jobs")
+    parser = argparse.ArgumentParser(description = 'Run dagmc_preproc.')
+    parser.add_argument('-j', '--jobs', type = int, default = 1,
+                        help = 'number of jobs')
     args = parser.parse_args()
     return args
 
 args = parse_args()
-inp_files = args.files
 jobs = args.jobs
+
+sat_files = commands.getstatusoutput('find . -type f -name *.sat')[1].split()
 
 if jobs > 1:
     pool = mp.Pool(processes = jobs)
 
-for inp_file in inp_files:
+for sat_file in sat_files:
     if jobs > 1:
-        pool.apply_async(run_dagmc_preproc, args = (inp_file,))
+        pool.apply_async(run_dagmc_preproc, args = (sat_file,))
     else:
-        run_mcnp2cad(inp_file)
+        run_dagmc_preproc(sat_file)
 
 if jobs > 1:
     pool.close()
--- DAG-MCNP5/scripts/run_mcnp2cad.py
+++ DAG-MCNP5/scripts/run_mcnp2cad.py
@@ -1,24 +1,29 @@
 import argparse
+import commands
+import os
 import sys
 from subprocess import call
 import multiprocessing as mp
 
 def run_mcnp2cad(inp_file):
-    sat_file = "geom_" + inp_file + ".sat"
-    call("mcnp2cad " + inp_file + " -o " + sat_file, shell = True)
+    dirname = os.path.dirname(os.path.dirname(inp_file))
+    name = os.path.basename(inp_file)
+    sat_file = os.path.join(dirname, 'Geom_sat', name + '.sat')
+    if not os.path.exists(os.path.join(dirname, 'Geom_sat')):
+        os.makedirs(os.path.join(dirname, 'Geom_sat'))
+    call('mcnp2cad ' + inp_file + ' -o ' + sat_file, shell = True)
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Run mcnp2cad.")
-    parser.add_argument("files", nargs = "*",
-                        help = "files on which to run mcnp2cad")
-    parser.add_argument("-j", "--jobs", type = int, default = 1,
-                        help = "number of jobs")
+    parser = argparse.ArgumentParser(description = 'Run mcnp2cad.')
+    parser.add_argument('-j', '--jobs', type = int, default = 1,
+                        help = 'number of jobs')
     args = parser.parse_args()
     return args
 
 args = parse_args()
-inp_files = args.files
 jobs = args.jobs
+
+inp_files = commands.getstatusoutput('find MCNP6 -type f')[1].split()
 
 if jobs > 1:
     pool = mp.Pool(processes = jobs)
