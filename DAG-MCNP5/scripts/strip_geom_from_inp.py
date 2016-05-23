6581b57b55bfec944a022316c94de0f9d6e65ad4
Lucas John Jacobson <ljjacobson@wisc.edu>
2016-05-23 15:44:24 -0500

    make sure 3rd entry on PRDMP is -2

--- DAG-MCNP5/scripts/strip_geom_from_inp.py
+++ DAG-MCNP5/scripts/strip_geom_from_inp.py
@@ -1,27 +1,86 @@
+import commands
 import os
-import sys
 from subprocess import call
 
-inp_files = sys.argv[1:]
+# Find all the files in the "Inputs_orig" directory
+inp_files = commands.getstatusoutput('find Inputs_orig -type f')[1].split()
 
-os.makedirs('orig')
+if not os.path.exists('Inputs'):
+    os.makedirs('Inputs')
 
-for inp_file in inp_files:
-    call('cp ' + inp_file + ' ' + 'orig', shell = True)
-    reader = open(inp_file, 'r')
-    lines = reader.readlines()
+for inp_orig in inp_files:
+    reader = open(inp_orig, 'r')
+    lines_in = reader.readlines()
     reader.close()
 
-    writer = open(inp_file, 'w')
-    writer.write(lines[0])
+    # Put the DAG-MCNP input file in the "Inputs" directory
+    inp_file = os.path.join('Inputs', os.path.basename(inp_orig))
+
     num_blank_lines = 0
-    for line in lines[1:]:
-        if num_blank_lines < 2:
-            writer.write('c ' + line)
-        elif line.strip().lower().startswith('imp'):
-            writer.write('c ' + line)
-        else:
-            writer.write(line)
-        if line.strip() == '':
+    prdmp_found = False
+    lines_out = []
+
+    # Write the title card
+    lines_out.append(lines_in[0])
+
+    # Loop over all the lines in the original input file
+    for line in lines_in[1:]:
+        line = line.strip()
+
+        # Blank line found
+        if line == '':
             num_blank_lines += 1
+
+        # MCNP does not read lines after the third blank line
+        if num_blank_lines >= 3:
+            break
+
+        # Comment out cell and surface cards
+        if num_blank_lines < 2:
+            lines_out.append('c %s\n' % line)
+
+        # Comment out importance card if it's found in the data card block
+        elif line.strip().lower().startswith('imp'):
+            lines_out.append('c %s\n' % line)
+
+        # Make sure the third entry on the PRDMP card is -2
+        elif line.strip().lower().startswith('prdmp'):
+            prdmp_found = True
+            prdmp_in = line.split()
+            prdmp_out = ['prdmp', 'j', 'j', '-2', '', '']
+            if prdmp_in[1].lower() == '2j':
+                j2 = 1
+                prdmp_out[1] = prdmp_in[1]
+                prdmp_out[2] = ''
+            else:
+                j2 = 0
+                prdmp_out[1] = prdmp_in[1]
+                prdmp_out[2] = prdmp_in[2]
+            if len(prdmp_in) > 4 - j2:
+                prdmp_out[4] = prdmp_in[4 - j2]
+            if len(prdmp_in) > 5 - j2:
+                prdmp_out[5] = prdmp_in[5 - j2]
+            prdmp_out = ' '.join([x for x in prdmp_out if x])
+            lines_out.append('%s\n' % prdmp_out)
+
+        # Write line as normal
+        else:
+            lines_out.append('%s\n' % line)
+
+    # Write the PRDMP card if it wasn't already written
+    if not prdmp_found:
+        lines_out.append('prdmp 2j -2\n')
+
+    writer = open(inp_file, 'wb')
+
+    # File is likely a real MCNP input file
+    if num_blank_lines in [2, 3]:
+        for line in lines_out:
+            writer.write(line)
+
+    # File is likely not a real MCNP input file
+    else:
+        for line in lines_in:
+            writer.write(line)
+
     writer.close()
