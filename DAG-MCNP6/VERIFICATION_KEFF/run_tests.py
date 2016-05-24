892cb802d4b9d6e20343bb0a8743e12559f60f5c
Lucas John Jacobson <ljjacobson@wisc.edu>
2016-05-24 16:58:06 -0500

    added xsdata info for ver_keff

--- DAG-MCNP6/VERIFICATION_KEFF/run_tests.py
+++ DAG-MCNP6/VERIFICATION_KEFF/run_tests.py
@@ -36,21 +36,22 @@ for name in names_to_run:
 
     test.physics = 'mcnp6'
 
-    # Directories
+    # Common
     test.dirs['orig'] = current_dir
     test.dirs['input'] = 'Inputs'
     test.dirs['sat'] = 'Geom_sat'
     test.dirs['gcad'] = 'Geom_h5m'
     test.dirs['result'] = 'Results/' + test.name
     test.dirs['temp'] = 'Templates/' + test.name
-
-    # Common input
     test.inputs['inp'] = test.name
     test.inputs['gcad'] = test.name + '.h5m'
     test.other['sat'] = test.name + '.sat'
-
-    # Common output
     test.outputs['outp'] = 'outp'
     test.outputs['mctal'] = 'mctal'
+
+    # Cross section data
+    test.dirs['xsdir'] = 'Inputs'
+    test.inputs['xsdir'] = 'keffdir1'
+    test.other['xslib'] = 'kefflib1'
 
 dagtest.run_multiple_tests(names_to_run, tests, args)
